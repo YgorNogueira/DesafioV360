3540291707b8e7c682019c4e447be9da2ba89c5e
YgorNogueira <ygornogueiramacedo@gmail.com>
2025-01-12 22:26:05 -0300

    CRUD on TO-DO lists completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,18 @@ class TasksController < ApplicationController
     end
   end
 
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to todo_path(@task.todo_id), notice: "Task was successfully updated."
+    else
+      redirect_to root_path, alert: "Task was not updated."
+    end
+  end
+
+
   private
 
   def task_params
